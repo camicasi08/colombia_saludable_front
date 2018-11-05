ac3649c39b2c071bba2d6ae32912fa68b8d06dee
Camilo Castillo <camiloandres4@gmail.com>
2018-11-05 16:50:36 -0500

    Ajustes grafo

--- src/app/pages/graph/graph.component.ts
+++ src/app/pages/graph/graph.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {BaseService} from '../../services/base.service';
+
 @Component({
   selector: 'cytograph',
   templateUrl: './graph.component.html',
@@ -7,6 +8,37 @@ import {BaseService} from '../../services/base.service';
 })
 
 export class CytodemoComponent implements OnInit {
+  hierarchialGraph = {nodes: [], links: []}
+  style = [
+    {
+        selector: 'node',
+        style: {
+            shape: 'ellipse',
+            'background-color': '#1a8cff',
+            label: 'data(name)'
+        }
+    }
+  ];
+/*   
+  layout ={
+    name: 'random',
+  
+    fit: true, // whether to fit to viewport
+    padding: 30, // fit padding
+    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+    animate: false, // whether to transition the node positions
+    animationDuration: 500, // duration of animation in ms if enabled
+    animationEasing: undefined, // easing of animation if enabled
+    animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
+    ready: undefined, // callback on layoutready
+    stop: undefined, // callback on layoutstop
+    transform: function (node, position ){ return position; } // transform a given node position. Useful for changing flow direction in discrete layouts 
+  };
+ */
+  layout ={
+    name:'grid'
+  }
+  
   /* private _graphData: any = {
     nodes: [
       {data: {id: 'j', name: 'Jerry', faveColor: '#6FB1FC', faveShape: 'circle'}},
@@ -48,7 +80,30 @@ export class CytodemoComponent implements OnInit {
         limit: this.limit,
         //ips: this.selectedIps,
     }
-    this.baseService.getMedicines(data).subscribe(result => this._graphData = result.json());
+    this.baseService.getMedicines(data).subscribe(result =>{
+
+        this._graphData = result.json()
+       
+        
+        /* console.log(this._graphData);
+        let nodes = [];
+        let links = [];
+        this._graphData.nodes.map(node =>{
+            nodes.push({
+              id:node.data.id,
+              label: node.data.label
+            })
+        })
+        this._graphData.edges.map(edge =>{
+          links.push({
+            source: edge.data.source,
+            target: edge.data.target
+          })
+      })
+        this.hierarchialGraph.nodes = nodes;
+        this.hierarchialGraph.links = links;
+        console.log(this.hierarchialGraph); */
+    });
   }
 
   get graphData(): any {
